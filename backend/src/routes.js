dc2d70db56e37ce49f7203278b2cf6a4695125b4
Andrei Dogaru <andreidogaru99@gmail.com>
2020-08-14 12:06:46 +0300

    Tags CRUD Operations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 
 //IMPORT ALL THE API ROUTES HERE
 const notes = require('./api/notes/notes.routes');
+const tags = require('./api/tags/tags.routes');
+
 //------------------------------
 
 
 const router = Router();
 
 router.use('/notes',notes);
+router.use('/tags',tags);
+
 
 module.exports = router;
